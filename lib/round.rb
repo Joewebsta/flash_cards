a030da0055fe3603a3d414410bdfe8cd472249b0
Joe Webster <joseph.scott.webster@gmail.com>
2020-08-08 19:00:29 -0400

    Create percent_correct_by_category method

--- lib/round.rb
+++ lib/round.rb
@@ -35,26 +35,15 @@ class Round
   end
 
   def percent_correct
-    number_correct.to_f / turns.count * 100
+    (number_correct / turns.count.to_f * 100).round(2)
   end
 
-  def percent_correct_by_category(category); end
-end
+  def percent_correct_by_category(category)
+    total_cards_by_category = turns.reduce(0) do |count, turn|
+      count += 1 if turn.card.category == category
+      count
+    end
 
-card_1 = Card.new('What is the capital of Alaska?', 'Juneau', :Geography)
-card_2 = Card.new('The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?', 'Mars', :STEM)
-card_3 = Card.new('Describe in words the exact direction that is 697.5° clockwise from due north?', 'North north west', :STEM)
-deck = Deck.new([card_1, card_2, card_3])
-round = Round.new(deck)
-new_turn = round.take_turn('Juneau')
-# p new_turn.class
-# p new_turn.correct?
-# p round.turns
-# p round.number_correct
-# p round.current_card
-round.take_turn('Venus')
-# p round.turns.count
-# p round.turns.last.feedback
-# p round.number_correct
-# p round.number_correct_by_category(:STEM)
-p round.percent_correct
+    (number_correct_by_category(category) / total_cards_by_category.to_f * 100).round(2)
+  end
+end
