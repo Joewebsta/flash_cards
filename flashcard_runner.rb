e66947e55471970ca4693f1255668883c500e0b4
Joe Webster <joseph.scott.webster@gmail.com>
2020-08-09 14:10:05 -0400

    Create deck and round

--- flashcard_runner.rb
+++ flashcard_runner.rb
@@ -1,5 +1,6 @@
 require './lib/card'
 require './lib/deck'
+require './lib/round'
 
 card1 = Card.new('What is 1 + 1?', 2, :math)
 card2 = Card.new('What is 5 * 5?', 25, :math)
@@ -8,4 +9,6 @@ card4 = Card.new('What language is Ruby on Rails written in?', 'Ruby', :code)
 card5 = Card.new('What does CSS stand for?', 'Cascading style sheets', :code)
 card6 = Card.new('What does OOP stand for?', 'Object oriented programming', :code)
 card7 = Card.new('What is the capital of MA?', 'Boston', :geography)
+
 deck = Deck.new([card1, card2, card3, card4, card5, card6, card7])
+round = Round.new(deck)
