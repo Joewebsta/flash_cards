78401c50b9777f9b041018c1981b23105dd619ee
Joe Webster <joseph.scott.webster@gmail.com>
2020-08-10 16:33:27 -0400

    Remove hard coded files and load cards from cards.txt file

--- flashcard_runner.rb
+++ flashcard_runner.rb
@@ -1,16 +1,11 @@
 require './lib/card'
 require './lib/deck'
 require './lib/round'
+require './lib/card_generator'
 
-card1 = Card.new('What is 1 + 1?', '2', :math)
-card2 = Card.new('What is 5 * 5?', '25', :math)
-card3 = Card.new('What is 100 / 10?', '10', :math)
-card4 = Card.new('What language is Ruby on Rails written in?', 'Ruby', :code)
-card5 = Card.new('What does CSS stand for?', 'Cascading style sheets', :code)
-card6 = Card.new('What does OOP stand for?', 'Object oriented programming', :code)
-card7 = Card.new('What is the capital of MA?', 'Boston', :geography)
-
-deck = Deck.new([card1, card2, card3, card4, card5, card6, card7])
+filename = './lib/cards.txt'
+cards = CardGenerator.new(filename).cards
+deck = Deck.new(cards)
 round = Round.new(deck)
 
 def start(round)
--- lib/card_generator.rb
+++ lib/card_generator.rb
@@ -1,8 +1,20 @@
 require_relative 'card'
 
-cards_file = File.open('cards.txt', 'r')
-cards_file.each do |line|
-  question, answer, category = line.chomp.split(',')
-  card = Card.new(question, answer, category)
-  p card
+class CardGenerator
+  attr_reader :filename
+
+  def initialize(filename)
+    @filename = filename
+  end
+
+  def open_file; end
+
+  def cards
+    cards_file = File.open(filename, 'r')
+    cards = cards_file.map do |line|
+      question, answer, category = line.chomp.split(',')
+      Card.new(question, answer, category.to_sym)
+    end
+    cards
+  end
 end
